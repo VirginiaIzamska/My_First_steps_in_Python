134605dad96d4faebb9e2d7cc3be2dfc1d8e0ff5
VirginiaIzamska <virginiya_petrova@yahoo.com>
2022-07-01 12:01:58 +0300

    Working_hours

---
+++ Working_hours.py
@@ -0,0 +1,13 @@
+
+part_of_day_and_night = int(input())
+day_of_week = str(input())
+
+# 10-18 часа,
+
+if 18 >= part_of_day_and_night >= 10:
+    if day_of_week == 'Monday' or day_of_week == 'Tuesday' or day_of_week == 'Wednesday' or day_of_week == 'Thursady' or day_of_week == 'Friday' or day_of_week == 'Saturday':
+        print('open')
+    else:
+        print('closed')
+else:
+    print('closed')
