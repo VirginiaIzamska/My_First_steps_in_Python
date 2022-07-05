d645863205fb9827e63867a673bf86f67c5f4654
VirginiaIzamska <virginiya_petrova@yahoo.com>
2022-07-05 15:24:24 +0300

    Car_to_go

---
+++ Car_to_go.py
@@ -0,0 +1,28 @@
+budget = float(input())
+season = input()
+class_car = ''
+type_car = ''
+price = 0
+if budget <= 100:
+    class_car = 'Economy class'
+    if season == 'Summer':
+        type_car = 'Cabrio'
+        price = budget * 0.35
+    elif season == 'Winter':
+        type_car = 'Jeep'
+        price = budget * 0.65
+elif budget <= 500:
+    class_car = 'Compact class'
+    if season == 'Summer':
+        type_car = 'Cabrio'
+        price = budget * 0.45
+    elif season == 'Winter':
+        type_car = 'Jeep'
+        price = budget * 0.80
+elif budget > 500:
+    class_car = 'Luxury class'
+    type_car = 'Jeep'
+    price = budget * 0.90
+
+print(class_car)
+print(f"{type_car} - {price:.2f}")
